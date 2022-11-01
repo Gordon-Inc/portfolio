d05711e9ba757f8b6a83f556b1dd69bcc4f296e3
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-11-01 13:43:04 -0700

    Changes files to JSX and builds header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,16 @@ import './header.css'
 export default function Header() {
     return (
         <div id="header">
-
+            <div className="nav">
+                <p className='top'></p>
+                <a href="#">Home</a>
+                <a href="#">Projects</a>
+                <a href="#">Accomplishments</a>
+                <a href="#">My Journey</a>
+                <a href="#">Tech Stack</a>
+                <a href="#">Documentation</a>
+                <p className='bottom'></p>
+            </div>
         </div>
     )
 }
