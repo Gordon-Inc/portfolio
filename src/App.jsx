7c857da64467b59e4ee410b3eaec14fc08df60c7
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-11-02 09:56:35 -0700

    sets up the basic proxy

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer"
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <Routes>
+
+        {/*
+        <Route path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/accomplishments' element={<Accomplishments />} />
+        <Route path='/myJourney' element={<MyJourney />} />
+        <Route path='/techStack' element={<TechStack />} />
+        <Route path='/documentation' element={<Documentation />} />
+      */}
+
+      </Routes>
+
       <Footer />
     </div>
   );
