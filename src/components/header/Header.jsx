9994a094bfd38eb842b7351d2d7167540138ae57
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-11-01 19:57:47 -0700

    Finishes the header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,15 +2,17 @@ import './header.css'
 export default function Header() {
     return (
         <div id="header">
-            <div className="nav">
-                <p className='top'></p>
-                <a href="#">Home</a>
-                <a href="#">Projects</a>
-                <a href="#">Accomplishments</a>
-                <a href="#">My Journey</a>
-                <a href="#">Tech Stack</a>
-                <a href="#">Documentation</a>
-                <p className='bottom'></p>
+            <div id="navbar">
+                <div className="nav">
+                    <p className='top'></p>
+                    <a href="#">Home</a>
+                    <a href="#">Projects</a>
+                    <a href="#">Accomplishments</a>
+                    <a href="#">My Journey</a>
+                    <a href="#">Tech Stack</a>
+                    <a href="#">Documentation</a>
+                    <p className='bottom'></p>
+                </div>
             </div>
         </div>
     )
