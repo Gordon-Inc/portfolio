bdaa36cf823e0aabcb0fd6ef8e3047711a1172ff
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-11-03 09:41:20 -0700

    renames file and finishes database schema

---
+++ db/migration.js
@@ -0,0 +1,148 @@
+/* eslint-disable no-undef */
+db.homePage.drop();
+db.projects.drop();
+db.accomplishments.drop();
+db.myJourney.drop();
+db.techStack.drop();
+
+db.createCollection("homePage", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            properties: {
+                name: {
+                    bsonType: "string",
+                    description: "First and Last name"
+                },
+                myJob: {
+                    bsonType: "string"
+                },
+                whoIAm: {
+                    bsonType: "string"
+                },
+                whatICanDo: {
+                    bsonType: "array"
+                },
+                recentWork: {
+                    bsonType: "string"
+                },
+                interests: {
+                    bsonType: "object",
+                    description: "3 properties: roles, companies, technologies"
+                }
+            }
+        }
+    }
+});
+
+db.createCollection("projects", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            properties: {
+                typeOf: {
+                    bsonType: "string",
+                },
+                name: {
+                    bsonType: "string",
+                },
+                img: {
+                    bsonType: "string",
+                },
+                techStack: {
+                    bsonType: "array",
+                },
+                url: {
+                    bsonType: "string",
+                },
+                description: {
+                    bsonType: "string",
+                },
+                likes: {
+                    bsonType: "number",
+                    description: "accumulate the number of likes"
+                },
+                comments: {
+                    bsonType: "object",
+                    description: "key will be name, value will be comment"
+                }
+            }
+        }
+    }
+});
+
+db.createCollection("accomplishments", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            properties: {
+                type: {
+                    bsonType: "string",
+                    description: "Certificate Course, Learning Course"
+                },
+                status: {
+                    bsonType: "string",
+                    description: "Complete, In Progress, or Backlog",
+                },
+                title: {
+                    bsonType: "string",
+                },
+                img: {
+                    bsonType: "string",
+                },
+                topics: {
+                    bsonType: "array",
+                },
+                description: {
+                    bsonType: "string",
+                }
+            }
+        }
+    }
+});
+
+db.createCollection("myJourney", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            properties: {
+                year: {
+                    bsonType: "number",
+                },
+                event: {
+                    bsonType: "string",
+                },
+                location: {
+                    bsonType: "string",
+                },
+                jobTitle: {
+                    bsonType: "string",
+                },
+                description: {
+                    bsonType: "string",
+                    description: "what happened during this event"
+                }
+            }
+        }
+    }
+});
+
+db.createCollection("techStack", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            properties: {
+                type: {
+                    bsonType: "string",
+                    description: "Front-End, Back-End or Developer Tools",
+                },
+                name: {
+                    bsonType: "string",
+                },
+                startDate: {
+                    bsonType: "string",
+                }
+            }
+        }
+    }
+});
