815e7c9abbeb78c25e0a1689a296f9e9216e1f9a
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-11-09 14:09:30 -0800

    imports the pool from the connection module

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const pool = require('./db/connection');
+const cors = require('cors');
+
 const PORT = process.env.PORT || 4000;
 
 app.use(express.static('build'));
 app.use(express.json());
-
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
