42c152f7b0522318289c273fe0f4830e3cf97160
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-11-09 13:41:09 -0800

    Adds pseudocode for app routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer"
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      <Routes>
+      {/*<Routes>
 
-        {/*
+
         <Route path='/' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/accomplishments' element={<Accomplishments />} />
         <Route path='/myJourney' element={<MyJourney />} />
         <Route path='/techStack' element={<TechStack />} />
         <Route path='/documentation' element={<Documentation />} />
-      */}
 
-      </Routes>
+
+    </Routes>*/}
 
       <Footer />
     </div>
