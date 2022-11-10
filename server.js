f8a1fa11a1ace75f9b14e32624c4386bb3666d25
Robert <96712943+bobbygrdn@users.noreply.github.com>
2022-11-10 14:18:30 -0800

    adds routes to each collection

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const pool = require('./db/connection');
 const cors = require('cors');
+const mongoose = require('mongoose');
+const homepageRouter = require('./routes/homepage')
+const testRouter = require('./routes/test')
+const accomplishmentsRouter = require('./routes/accomplishments');
+const myJourneyRouter = require('./routes/myjourney')
+const projectsRouter = require('./routes/projects')
+const techStackRouter = require('./routes/techstack')
+
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Connected to Database'))
 
 const PORT = process.env.PORT || 4000;
 
@@ -10,6 +24,11 @@ app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
 
-app.listen(PORT, () => {
-    console.log(`Listening on port: ${PORT}`)
-})
+app.use('/test', testRouter)
+app.use('/home', homepageRouter)
+app.use('/accomplishments', accomplishmentsRouter)
+app.use('/myJourney', myJourneyRouter)
+app.use('/projects', projectsRouter)
+app.use('/techStack', techStackRouter)
+
+app.listen(PORT, () => console.log('Server Started'))
